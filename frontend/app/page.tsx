5433f6e2e2cdb13595c919e6c71be5281c0ac88f
Максим <rmd09@yandex.ru>
2026-01-26 09:57:13 +0300

    Вынесение формы авторизации в отдельный компонент

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,44 +4,13 @@ import Image from "next/image";
 import { FocusEvent, FormEvent, FormEventHandler, useEffect, useState } from "react";
 import { endpoints } from "./config/endpoints"
 import { loginUser } from "./tools/auth";
+import { AuthForm } from "./components/AuthForm/AuthForm";
 
 export default function Home() {
-  const [test, setTest] = useState("");
-  const [authInfo, setAuthInfo] = useState({ username: "", password: "" });
-
-  const onSubmitHandler = async(event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const response = await loginUser(authInfo);
-    if (response.ok) {
-      setTest(JSON.stringify(response.user));
-    } else {
-      setTest(response.error ?? "");
-    }
-  };
-
-  const inputHandler = (event: FocusEvent<HTMLInputElement, Element>) => {
-    const temp = {...authInfo, [event.target.name]: event.target.value }
-    
-    setAuthInfo(temp);
-  }
 
   return (
     <div>
-      <form onSubmit={onSubmitHandler}>
-        <h3>Введите имя пользователя</h3>
-        <input onBlur={inputHandler} type="text" name="username" />
-        <h3>Введите пароль</h3>
-        <input onBlur={inputHandler} type="text" name="password"/>
-        <button type="submit">Авторизоваться</button>
-
-        {
-          test && (
-            <>
-            <p>Ответ: {test}</p>
-            </>
-          )
-        }
-      </form>
+      <AuthForm />
     </div>
   );
 }
