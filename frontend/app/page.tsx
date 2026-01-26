eaec1809792de0b4c941eaa37132ca33a48fd32b
Максим <123485868+rmd09@users.noreply.github.com>
2026-01-26 10:03:44 +0300

    Frontend/структура и конфигурация (#6)
    * Создание endpoints.ts
    
    + тестирование. Работает)
    
    * Авторизация пользователя с сохранением jwt
    
    * Изменения параметров запуска для dev и prod
    
    * Загрузка файла .env для фронтенда
    
    Всё равно там ничего секретного нет
    
    * Изменение структуры
    
    * Базовая форма авторизации
    
    * Вынесение формы авторизации в отдельный компонент
    
    * Изменение формы на пароль

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,31 +1,16 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { FocusEvent, FormEvent, FormEventHandler, useEffect, useState } from "react";
+import { endpoints } from "./config/endpoints"
+import { loginUser } from "./tools/auth";
+import { AuthForm } from "./components/AuthForm/AuthForm";
 
 export default function Home() {
-  const [test, setTest] = useState("");
-
-  useEffect(() => {
-    console.log("test");
-    fetch("/api/api/login", {
-      body: JSON.stringify({username: "admin", password: "password"}),
-      headers: {
-        "Content-Type": "application/json"
-      },
-      method: "POST"
-    }).then(async(value) => {
-      const json = await value.json();
-      console.log(json);
-      setTest(json.access_token);
-    });
-  }, [])
 
   return (
     <div>
-      {test}
-      <br/>
-      <h1>Сука финалочка (ещё разок)</h1>
+      <AuthForm />
     </div>
   );
 }
