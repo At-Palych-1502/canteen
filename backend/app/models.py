2fe8644cc63e26aaa5f0fa5f6d925cc1810940b2
xchose31 <rte3001@vk.com>
2026-01-26 08:23:03 +0300

    пофикшена ошибка импорта, мешающая запустить приложение

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON
 from sqlalchemy.orm import relationship
-from ..app import db
+from .extensions import db
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
