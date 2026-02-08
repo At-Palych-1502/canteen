9350b553bcc28832582966a5500c2910e08800b1
xchose31 <rte3001@vk.com>
2026-02-09 00:52:59 +0300

    добавлена фильтрация пользователей и изменена документация

--- backend/app/api/business.py
+++ backend/app/api/business.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..models import User, Transaction, Meal, Order
+from flask_jwt_extended import jwt_required
+from ..models import Meal, User
 import datetime
 from .. import db
 from ..utils import role_required
+from sqlalchemy import or_
 
 bp = Blueprint('business', __name__)
 
@@ -18,3 +19,38 @@ def get_menu():
     for meal in meals:
         sl.append(meal.to_dict())
     return jsonify(sl), 200
+
+@bp.route('/users/filter')
+@jwt_required()
+@role_required(['admin'])
+def filter_users():
+    args = request.args
+    username = args.get('username')
+    email = args.get('email')
+    role = args.get('role')
+
+    page = request.args.get('page', 1, type=int)
+
+    users_query = User.query.filter(
+        or_(
+            User.username.ilike(f'%{username}%'),
+            User.email.ilike(f'%{email}%'),
+            User.role.ilike(f'%{role}%')
+        )
+    )
+
+    pagination = users_query.paginate(
+        page=page,
+        per_page=8,
+        error_out=False
+    )
+    users = pagination.items
+    return jsonify({"users": [user.to_dict() for user in users],
+                    "pagination": {
+                        "page": pagination.page,
+                        "pages": pagination.pages,
+                        "per_page": pagination.per_page,
+                        "total": pagination.total,
+                        "has_next": pagination.has_next,
+                        "has_prev": pagination.has_prev
+                    }})
