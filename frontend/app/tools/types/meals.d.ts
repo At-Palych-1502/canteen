e87239bcb3b883ee398a1f0fd5415f815669dbda
Максим <rmd09@yandex.ru>
2026-02-09 18:28:28 +0300

    fix

--- frontend/app/tools/types/meals.d.ts
+++ frontend/app/tools/types/meals.d.ts
@@ -8,11 +8,15 @@ export interface IMealCreateArg {
 	day_of_week: 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday';
 }
 
-export interface IMeal extends IMealCreateArg {
-	id: number;
+export interface IMeal {
+	name: string;
+	price: number;
+	day_of_week: string;
+	quantity: number;
 
 	dishes: IDish[];
 }
+
 
 export type IMealUpdateReq = {
 	id: number;
