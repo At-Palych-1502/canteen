5dad1db076cccfcd2bf3192c04d12c59d94016a9
xchose31 <rte3001@vk.com>
2026-02-09 12:42:37 +0300

    добавлены отзывы

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -5,6 +5,7 @@ from .ingredient import bp as ing_bp
 from .balance import bp as balance_bp
 from .meal import bp as meal_bp
 from .business import bp as business_bp
+from .reviews import bp as review_bp
 
 api = Blueprint('api', __name__, url_prefix='/api')
 api.register_blueprint(auth_bp)
@@ -13,3 +14,4 @@ api.register_blueprint(ing_bp)
 api.register_blueprint(balance_bp)
 api.register_blueprint(meal_bp)
 api.register_blueprint(business_bp)
+api.register_blueprint(review_bp)
