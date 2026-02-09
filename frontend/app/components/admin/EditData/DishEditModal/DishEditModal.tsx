e5465a48e3e480cf145028d5fe36e463f9f7a935
Roman <romann.mihaylov@gmail.com>
2026-02-09 15:16:15 +0300

    feat: сделал сохранение блюд

--- frontend/app/components/admin/EditData/DishEditModal/DishEditModal.tsx
+++ frontend/app/components/admin/EditData/DishEditModal/DishEditModal.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { IDishExtended } from '@/app/tools/types/mock';
+import React, { useEffect, useState } from 'react';
 import Styles from './DishEditModal.module.css';
 import { IDish } from '@/app/tools/types/dishes';
 import {
@@ -22,22 +21,52 @@ const DishEditModal: React.FC<DishEditModalProps> = ({
 	const [updateDish, { error, isLoading: isUpdateLoading }] =
 		useUpdateDishMutation();
 
-	console.log(error, isUpdateLoading);
-
-	const { data: dish, isLoading } = useGetDishByIdQuery(id);
+	const { data: dish, isLoading, refetch } = useGetDishByIdQuery(id);
 	const [formData, setFormData] = React.useState<IDish | null>(null);
+
+	const [showIngredientsSelector, setShowIngredientsSelector] = useState(false);
+
+	const [filteredIngredients, setFilteredIngredients] = useState<IIngredient[]>(
+		[],
+	);
+	const [searchTerm, setSearchTerm] = useState('');
 
 	useEffect(() => {
 		if (typeof dish !== 'undefined') setFormData(dish.data);
-	}, [dish]);
+		if (typeof ingredients !== 'undefined') setFilteredIngredients(ingredients);
+	}, [dish, ingredients]);
+
+	useEffect(() => {
+		if (searchTerm !== '') {
+			const filtered = ingredients.filter(dish =>
+				dish.name.toLowerCase().includes(searchTerm.toLowerCase()),
+			);
+			setFilteredIngredients(filtered);
+		} else {
+			setFilteredIngredients(ingredients || []);
+		}
+	}, [searchTerm, ingredients, dish]);
 
 	if (!dish || !formData) return null;
 
 	const handleChange = (
-		field: keyof IDishExtended,
+		field: keyof IDish,
 		value: string | number | boolean,
 	) => {
 		setFormData(prev => (prev ? { ...prev, [field]: value } : null));
+	};
+
+	const handleRemoveIngredient = (ingredientId: number) => {
+		setFormData(prev => {
+			if (!prev) return null;
+
+			return {
+				...prev,
+				ingredients: (prev.ingredients || []).filter(
+					d => d.id !== ingredientId,
+				),
+			};
+		});
 	};
 
 	const handleSubmit = (e: React.FormEvent) => {
@@ -54,10 +83,34 @@ const DishEditModal: React.FC<DishEditModalProps> = ({
 		});
 
 		onClose(true);
+		refetch();
 	};
 
-	const handleRemoveIngredient = (id: number) => {
-		console.log('удаление', id);
+	const handleAddIngredient = (ingredientId: number) => {
+		const ingredient = ingredients.find(d => d.id === ingredientId);
+
+		if (!ingredient || !showIngredientsSelector) return;
+
+		setFormData(prev => {
+			if (!prev) return null;
+			const existingIngredients = prev.ingredients;
+			const ingredientExists = (formData.ingredients || []).some(
+				d => d.id === ingredientId,
+			);
+
+			if (ingredientExists || !existingIngredients) return prev;
+
+			const newIngredient = {
+				id: ingredient.id,
+				name: ingredient.name,
+			};
+
+			return {
+				...prev,
+				ingredients: [...existingIngredients, newIngredient],
+			};
+		});
+		setSearchTerm('');
 	};
 
 	return (
@@ -100,60 +153,85 @@ const DishEditModal: React.FC<DishEditModalProps> = ({
 									}
 								/>
 							</div>
+						</div>
+						<div className={Styles['ingredients']}>
 							<div className={Styles.ingredientsList}>
 								<label>Ингридиенты:</label>
-								{ingredients.map(ingredient => (
-									<div key={ingredient.id} className={Styles.ingredientItem}>
-										<span>{ingredient.name}</span>
+								<div className={Styles['ingredientsWrapper']}>
+									{!formData.ingredients ? (
+										<p>Не удалось загрузить ингридиенты</p>
+									) : (
+										formData.ingredients.map(ingredient => (
+											<div
+												key={ingredient.id}
+												className={Styles.ingredientItem}
+											>
+												<span>{ingredient.name}</span>
+												<button
+													type='button'
+													onClick={() => handleRemoveIngredient(ingredient.id)}
+													className={Styles.removeIngredientBtn}
+												>
+													&times;
+												</button>
+											</div>
+										))
+									)}
+								</div>
+							</div>
+							<div className={Styles['addIngredients']}>
+								<button
+									type='button'
+									onClick={() => setShowIngredientsSelector(true)}
+									className={Styles.addIngredientBtn}
+								>
+									+ Добавить ингридиент
+								</button>
+								{showIngredientsSelector === true && (
+									<div className={Styles.ingredientsSelector}>
+										<input
+											type='text'
+											placeholder='Поиск ингридиента...'
+											value={searchTerm}
+											onChange={e => setSearchTerm(e.target.value)}
+											className={Styles.searchInput}
+											autoFocus
+										/>
+										<div className={Styles.ingredientsOptions}>
+											{filteredIngredients.map(ingredient => {
+												const isAdded = (formData.ingredients || []).some(
+													d => d.id === ingredient.id,
+												);
+
+												return (
+													<div
+														key={ingredient.id}
+														onClick={() =>
+															!isAdded && handleAddIngredient(ingredient.id)
+														}
+														className={`${Styles.ingredientsOption} ${isAdded ? Styles.ingredientsOptionDisabled : ''}`}
+													>
+														{ingredient.name}
+														{isAdded && (
+															<span className={Styles.addedBadge}>✓</span>
+														)}
+													</div>
+												);
+											})}
+										</div>
 										<button
 											type='button'
-											onClick={() => handleRemoveIngredient(ingredient.id)}
-											className={Styles.removeIngredientBtn}
+											onClick={() => {
+												setShowIngredientsSelector(false);
+												setSearchTerm('');
+											}}
+											className={Styles.cancelBtn}
 										>
-											&times;
+											Отмена
 										</button>
 									</div>
-								))}
+								)}
 							</div>
-							{/* {showDishSelector === type && (
-								<div className={Styles.dishSelector}>
-									<input
-										type='text'
-										placeholder='Поиск блюда...'
-										value={searchTerm}
-										onChange={e => setSearchTerm(e.target.value)}
-										className={Styles.searchInput}
-										autoFocus
-									/>
-									<div className={Styles.dishOptions}>
-										{filteredDishes.map(dish => {
-											const isAdded = dishes.some(d => d.id === dish.id);
-											return (
-												<div
-													key={dish.id}
-													onClick={() => !isAdded && handleAddDish(dish.id)}
-													className={`${Styles.dishOption} ${isAdded ? Styles.dishOptionDisabled : ''}`}
-												>
-													{dish.name}
-													{isAdded && (
-														<span className={Styles.addedBadge}>✓</span>
-													)}
-												</div>
-											);
-										})}
-									</div>
-									<button
-										type='button'
-										onClick={() => {
-											setShowDishSelector(null);
-											setSearchTerm('');
-										}}
-										className={Styles.cancelBtn}
-									>
-										Отмена
-									</button>
-								</div>
-							)} */}
 						</div>
 						<div className={Styles.actions}>
 							<button
