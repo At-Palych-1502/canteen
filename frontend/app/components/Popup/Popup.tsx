87a34146c3c3f3d858486ce574f3b02db4528e68
Максим <rmd09@yandex.ru>
2026-02-15 22:58:02 +0300

    fix: доработка Popup

--- frontend/app/components/Popup/Popup.tsx
+++ frontend/app/components/Popup/Popup.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Styles from './Popup.module.css';
 
 interface Props {
@@ -6,9 +7,18 @@ interface Props {
 }
 
 export function Popup(props: Props) {
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+	}, []);
+
+	const close = () => {
+		document.body.style.overflow = '';
+		props.closePopup();
+	}
+
 	return (
 		<>
-			<div onClick={props.closePopup} className={Styles['blur']}></div>
+			<div onClick={close} className={Styles['blur']}></div>
 			<div className={Styles['main']}>{props.children}</div>
 		</>
 	);
